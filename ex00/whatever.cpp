8e505cd420338642dd3f799e8e69027de51a670b
KHALIL DRISSI <galilai@MacBook-galilai.local>
2015-01-14 16:41:38 +0100

    ex0

---
+++ ex00/whatever.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+template<typename T>
+void swap(T& a, T& b) {
+  T c = a;
+  a = b;
+  b = c;
+}
+template<typename T>
+const T & min(const T & a, const T & b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+template<typename T>
+const T & max(const T & a, const T & b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+int main (void)
+{
+int a = 2;
+int b = 3;
+ ::swap(a,b);
+std::cout << "a = " << a << ", b = "  << b << std::endl;
+std::cout <<"min( a, b ) = " << ::min(a,b) << std::endl;
+std::cout << "max( a, b ) = " << ::max(a,b)<<std::endl;
+std::string c="chaine1";
+std::string d="chaine2";
+::swap(c,d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " <<::min(c,d) << std::endl;
+std::cout << "max( c, d ) = " << ::max(c,d) << std::endl;
+return 0 ;
+}
